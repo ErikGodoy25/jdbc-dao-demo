e074ed6144bf416ad7ae711aacc5407101bfbe84
Erik Godoy <erik.sgodoy@gmail.com>
2021-04-02 21:31:47 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,23 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department dp = new Department(1, "Teste");
-		
-		Seller sl = new Seller(1, "Erik", "erik@gmail.com", new Date(), 3000.0, dp);
-		
-		System.out.println(sl);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(1);
+		
+		System.out.println(seller);
 
 	}
 
